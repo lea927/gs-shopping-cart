2ccfc92e687f58c0bda85a1ae7333ef27f511cbf
lea927 <lxthym@icloud.com>
2021-10-26 17:14:52 +0800

    Updates relationship of brand to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_041407) do
+ActiveRecord::Schema.define(version: 2021_10_26_090512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,9 @@ ActiveRecord::Schema.define(version: 2021_10_26_041407) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "brand_id"
+    t.bigint "brand_id", null: false
+    t.index ["brand_id"], name: "index_products_on_brand_id"
   end
 
+  add_foreign_key "products", "brands"
 end
