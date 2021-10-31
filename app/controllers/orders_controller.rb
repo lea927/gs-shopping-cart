91c6ef361208a46aff510c5b03a85ddef8f0ed7b
lea927 <lxthym@icloud.com>
2021-10-31 16:26:03 +0800

    Adds resources for orders and add function to delete an order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:destroy,:show]
+
   def index
     @orders = Order.all
   end
 
   def new
     @order = Order.new
+  end
+
+  def show
+
   end
 
   def create
@@ -15,11 +21,17 @@ class OrdersController < ApplicationController
   end
 
   def destroy
+    @order.destroy
+    redirect_to products_path
   end
 
   private
 
   def order_params
     params.permit(:name, :price)
+  end
+
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   get '/cart', to: 'orders#index'
-  get 'orders/new'
-  post 'orders/create'
-  get 'orders/destroy'
   root 'products#index' 
   get '/products', to: 'products#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :orders, only: [:create, :destroy, :new, :show]
 end
