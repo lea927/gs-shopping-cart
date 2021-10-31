23a654337c4e07939e6a989d124f9792bbcafa5d
lea927 <lxthym@icloud.com>
2021-10-31 17:19:15 +0800

    Updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create!([
+  {name: 'Canned Seafood'}
+])
+
+Brand.create!([
+  {name: '555'}
+])
+
+Product.create!([
+  {name:'555 Sardines in Tomato Sauce with Chili Can - 155g',barcode:748485200026,price: 15.26, brand_id: 1, category_id: 1},
+  {name:'555 Sardines in Tomato Sauce Can - 155g',barcode:748485200019,price: 15.47, brand_id: 1, category_id: 1},
+  {name:'555 Tuna Adobo Can - 155g',barcode:748485700014,price: 21.4, brand_id: 1, category_id: 1},
+  {name:'555 Tuna Afritada Can - 155g',barcode:748485700021,price: 21.13, brand_id: 1, category_id: 1},
+  {name:'555 Tuna Caldereta Can - 155g',barcode:748485700038,price: 21.72, brand_id: 1, category_id: 1},
+  {name:'555 Tuna Flakes Oil Can - 155g',barcode:748485700083,price: 24.09, brand_id: 1, category_id: 1},
+  {name:'555 Tuna Hot and Spicy Can - 155g',barcode:748485700052,price: 23.95, brand_id: 1, category_id: 1},
+  {name:'555 Tuna Mechado Can - 155g',barcode:748485700045,price: 21.89, brand_id: 1, category_id: 1},
+])
